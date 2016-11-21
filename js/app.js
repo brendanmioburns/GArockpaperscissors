af372c1bd36af18af1362ce020b778c81bd85b0e
Brendan Burns <boardslide03@Brendans-MacBook-Pro-2.local>
2016-11-20 22:39:21 -0800

    last edits

--- js/app.js
+++ js/app.js
@@ -1,19 +1,47 @@
 $(document).ready(function() {
 
     // randomize computer's choice on page load
-    var computerChoice = math.Random();
+    var randomNumber = Math.random();
 
-    if (computerChoice =< 0.33) {
-        computerChoice = "rock";
-    } else if (0.66 >= computerChoice && computerChoice >= 0.34) {
-        computerChoice = "paper";
+    if (randomNumber < 0.33) {
+        var computerChoice = "rock";
+    } else if (randomNumber >= 0.33 && randomNumber < 0.66) {
+        var computerChoice = "paper";
     } else {
-        computerChoice = "scissors";
+        var computerChoice = "scissors";
     }
 
+    console.log(computerChoice);
+
     // add click events to user choices
-    $(this).on("click", function() {
-        var userChoice = .attr()
+    $("#rock").on("click", function() {
+        if (computerChoice === "scissors") {
+            $(this).addClass("success green-background");
+        } else if (computerChoice === "rock") {
+            alert("Tie game!");
+        } else {
+            alert("You lost!");
+        }
+    });
+
+    $("#paper").on("click", function() {
+        if (computerChoice === "rock") {
+            $(this).addClass("success green-background");
+        } else if (computerChoice === "paper") {
+            alert("Tie game!");
+        } else {
+            alert("You lost!");
+        }
+    });
+
+    $("#scissors").on("click", function() {
+        if (computerChoice === "paper") {
+            $(this).addClass("success green-background");
+        } else if (computerChoice === "scissors") {
+            alert("Tie game!");
+        } else {
+            alert("You lost!");
+        }
     });
 
 });
